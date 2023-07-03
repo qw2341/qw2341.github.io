98c54461fd0a357183c4adcd765ea60a88efd241
Qingqi Wu <qw2341@tc.columbia.edu>
2023-07-03 15:14:39 -0700

    bug fixed

--- get_portfolio_folders.php
+++ get_portfolio_folders.php
@@ -1,5 +1,5 @@
-<?php
+var folders = <?php
 $dir = './portfolio/';
 $folders = array_diff(scandir($dir), array('..', '.'));
-echo 'callback(' . json_encode($folders) . ')';
-?>
+echo json_encode($folders);
+?>;
